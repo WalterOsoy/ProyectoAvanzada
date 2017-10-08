c3fed9bb122bb12b7a985b1da16bdd9461b05c8e
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2017-10-08 15:57:06 -0600

    actualizando proyecto

---
+++ proyecto/proyecto/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto
+{    
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string password = textBox2.Text;
+            bool permitir = Goathemala.buscar(user, password);
+            if (permitir=true)
+            {
+                this.Hide();
+                Principal obj = new Principal();
+                obj.Show();
+            }
+            else
+            {
+                textBox3.Text = "datos invalidos";
+            }                       
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SignUp sign = new SignUp();
+            sign.Show();            
+        }
+    }
+}
---
+++ proyecto/proyecto/Goathemala.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto
+{
+    class Goathemala
+    {
+        private static Usuario[] congreso = new Usuario[100];        
+        private static int cont = 0;
+        //agrega un nuevo usuario 
+        public static void addPolitico(Usuario politicos)
+        {
+            congreso[cont] = politicos;
+            cont++;
+        }
+        //codigo para el inicio de secion
+        public static bool buscar(string name, string password)
+        {            
+            bool permitido = false;
+            for (int i = 0; i < congreso.Length; i++)
+            {
+                if (name == congreso[i].getUser())
+                {
+                    if (password.Equals(congreso[i].getPassword()))
+                    {
+                        permitido = true;
+                    }
+                }
+            }            
+            return permitido;
+        }
+    }
+}
